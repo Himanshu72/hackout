abb52fdc250dfcb4acdea63492705e40e77bc421
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-10-05 00:18:09 +0530

    Completed Sructure

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Express App Starts Here
-const createError = require("http-errors");
+
 const express = require("express");
+const createError = require("http-errors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -9,7 +10,6 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -24,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
