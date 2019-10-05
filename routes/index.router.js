eb08e90854f0b35c4d192f614d0d32107f400eca
unknown <hjoshi115@gmail.com>
2019-10-05 07:54:52 +0530

    Resolving conflicts by resetting and merging different branch

--- routes/index.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const indexRouter = express.Router();
-
-/* GET home page. */
-indexRouter.get("/", (req, res) => {
-  res.render("pages/index", {});
-});
-
-module.exports = indexRouter;
